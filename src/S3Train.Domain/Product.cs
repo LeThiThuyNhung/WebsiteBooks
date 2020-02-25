af2b7efb5f49c7c20b498f88f36951bdddfbd3fb
THUYBN <Thuy31081998@gmail.com>
2020-02-25 15:11:49 +0700

    Create page list product

--- src/S3Train.Domain/Product.cs
+++ src/S3Train.Domain/Product.cs
@@ -5,12 +5,17 @@ namespace S3Train.Domain
     public class Product : EntityBase
     {
         public Guid CategoryId { get; set; }
+        public Guid PublisherId { get; set; }
         public string Name { get; set; }
         public string Summary { get; set; }
         public decimal Price { get; set; }
         public string ImagePath { get; set; }
+        public string Barcode { get; set; }
+        public int ReleaseYear { get; set; }
+        public int Amount { get; set; }
         public int? Rating { get; set; }
 
         public virtual Category Category { get; set; }
+        public virtual Publisher Publisher { get; set; }
     }
 }
