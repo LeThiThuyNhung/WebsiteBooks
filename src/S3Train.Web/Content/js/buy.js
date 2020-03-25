f3d10865ba20d1f1778b240fa0e9c068ca1a2efd
ThuyNhung <thuynhung2120@gmail.com>
2020-03-26 01:09:52 +0700

    update src

--- src/S3Train.Web/Content/js/buy.js
+++ src/S3Train.Web/Content/js/buy.js
@@ -1,13 +1,24 @@
-﻿$(document).ready(function () {
-    $('.btn-default').click(function () {
-        var id = $(this).data("id");
-        $.ajax({
-            type: "POST",
-            url: '/Cart/Buy',
-            data: { Id: id },
-            success: function (data) {
-                alert("Add to cart successfully")
-            }
+﻿<script>
+    $(document).ready(function () {
+        $('.btn-default').click(function () {
+            var id = $(this).data("id");
+            $.ajax({
+                type: "POST",
+                url: '/Cart/Buy',
+                data: { Id: id },
+                success: function (data) {
+                }
+            });
         });
     });
-});
+    function myFunction() {
+      // Get the snackbar DIV
+      var x = document.getElementById("snackbar");
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+        setTimeout(function () {x.className = x.className.replace("show", ""); }, 3000);
+}
+</script>
